d3e3e6867d70c98adb6accc460f1c0e8acebbd24
Abraham Williams + Pearl Latteier <dev+abraham+pearlbea@bendyworks.com>
2016-06-11 11:22:54 -0500

    initial refactoring of js

--- public/js/app.js
+++ public/js/app.js
@@ -1,44 +1,116 @@
-var camera, controls, scene, grid, renderer, ws;
+var BGL = (function(THREE){
+  'use strict';
 
-var gridOptions = {
-  layers: 10,
-  color: 0x0000ff
-};
+  var controls, scene, grid, renderer, ws,
+    gridOptions = {
+      layers: 10,
+      color: 0x0000ff
+    };
 
-function createCenterCell() {
-  var geometry = new THREE.SphereGeometry(0.25, 16, 16);
-  var material = new THREE.MeshBasicMaterial({ color: 0xF44336 });
-  return new THREE.Mesh(geometry, material);
-}
+  var createScene = function() {
+    var scene = new THREE.Scene();
+    BGL.renderer = new THREE.WebGLRenderer({ alpha: true });
 
-function createScene() {
-  // define
-  var scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera( 90, window.innerWidth / window.innerHeight, 1, 1000);
-  renderer = new THREE.WebGLRenderer({ alpha: true });
+    BGL.renderer.setSize(window.innerWidth - 100, window.innerHeight - 100);
 
-  // configure
-  renderer.setSize(window.innerWidth - 100, window.innerHeight - 100);
+    document.getElementById('canvas').appendChild(BGL.renderer.domElement);
 
-  // add
-  document.getElementById('canvas').appendChild(renderer.domElement);
+    BGL.controls = new THREE.OrbitControls(BGL.camera.instance(), BGL.renderer.domElement);
+    BGL.controls.enableDamping = true;
+    BGL.controls.dampingFactor = 0.25;
 
-  controls = new THREE.OrbitControls(camera, renderer.domElement);
-  controls.enableDamping = true;
-  controls.dampingFactor = 0.25;
+    return scene;
+  };
 
-  return scene;
-}
+  return {
+    createScene: createScene,
+    controls: controls,
+    gridOptions: gridOptions,
+    grid: grid,
+    renderer: renderer
+  };
+}(THREE));
+
+BGL.camera = (function() {
+  'use strict';
+  var instance;
+
+  var position = function(position) {
+    instance.position.z = position;
+  };
+
+  var create = function() {
+    var zoom = window.innerWidth / window.innerHeight;
+    instance = new THREE.PerspectiveCamera(90, zoom, 1, 1000);
+    return instance;
+  };
+
+  var get = function() {
+    return instance || create();
+  };
+
+  return {
+    instance: get,
+    position: position
+  };
+}());
+
+BGL.cells = (function(THREE){
+  'use strict';
+
+  var processCell = function(coordinates) {
+    var cells = [];
+    for (var key in coordinates) {
+      if (coordinates[key] === 'true') {
+        cells.push(create(key, coordinates[key]));
+      }
+    }
+    return cells;
+  };
+
+  var build = function(cell) {
+    var geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    var cube = new THREE.Mesh( geometry, material );
+    cube.position.x = cell.x;
+    cube.position.y = cell.y;
+    cube.position.z = cell.z;
+    return cube;
+  };
+
+  var create = function(key) {
+    var indexes = key.split(':');
+    var coordinates = {
+      x: Number(indexes[0]),
+      y: Number(indexes[1]),
+      z: Number(indexes[2])
+    };
+    return build(coordinates);
+  };
+
+  var createCenter = function() {
+    var geometry = new THREE.SphereGeometry(0.25, 16, 16);
+    var material = new THREE.MeshBasicMaterial({ color: 0xF44336 });
+    return new THREE.Mesh(geometry, material);
+  };
+
+  return {
+    processCell: processCell,
+    createCenter: createCenter
+  };
+}(THREE));
+
+
 
 function animate() {
   requestAnimationFrame(render);
-  controls.update();
+  BGL.controls.update();
   render();
 }
 
 function render() {
   requestAnimationFrame(render);
-  renderer.render(scene, camera);
+  BGL.renderer.render(scene, BGL.camera.instance());
 }
 
 function createAGrid(opts) {
@@ -55,20 +127,20 @@ function clearScene() {
 }
 
 function onMessage(m) {
-  var data = JSON.parse(m.data)
+  var data = JSON.parse(m.data);
   console.log('Recieved message', m);
   if (data.result === 'coordinates') {
     clearScene();
-    var cells = processCell(data.data);
+    var cells = BGL.cells.processCell(data.data);
     cells.forEach(function(cell) {
-      grid.add(cell);
+      BGL.grid.add(cell);
     });
     animate();
     setTimeout(function() {
       if (!isPaused()) {
         push({action: 'tick'});
       }
-    }, 500)
+    }, 500);
   }
 }
 
@@ -76,48 +148,14 @@ function isPaused() {
   return document.getElementById('pause').active;
 }
 
-function processCell(coordinates) {
-  var cells = [];
-  for (var key in coordinates) {
-    if (coordinates[key] === 'true') {
-      cells.push(createCell(key, coordinates[key]));
-    }
-  }
-  return cells;
-}
-
-function buildCell(cell) {
-  var geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-  var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-  var cube = new THREE.Mesh( geometry, material );
-  cube.position.x = cell.x;
-  cube.position.y = cell.y;
-  cube.position.z = cell.z;
-  return cube;
-}
-
-function createCell(key) {
-  var indexes = key.split(':');
-  var coordinates = {
-    x: Number(indexes[0]),
-    y: Number(indexes[1]),
-    z: Number(indexes[2])
-  }
-  return buildCell(coordinates);
-}
-
 function connectToSocket(openCallback) {
   ws = new WebSocket('ws://' + window.location.host + '/live');
   ws.onmessage = onMessage;
-  ws.onclose = function()  { console.log('websocket closed'); }
+  ws.onclose = function()  { console.log('websocket closed'); };
 }
 
 function requestCells(config) {
   push({action: 'start', layers: config.layers, fillPercent: config.fillPercent});
-}
-
-function postionCamera(position) {
-  camera.position.z = position;
 }
 
 function getConfig() {
@@ -126,14 +164,14 @@ function getConfig() {
   return {
     layers: layers.value,
     fillPercent: fillPercent.value
-  }
+  };
 }
 
 function toggleLife() {
   var start = document.getElementById('start');
   var pause = document.getElementById('pause');
   if (start.active) { // .active is the previous value as button transition has not happend yet
-    push({action: 'stop'})
+    push({action: 'stop'});
     start.textContent = 'Start';
     start.classList.remove('red');
     start.classList.add('green');
@@ -142,7 +180,7 @@ function toggleLife() {
   } else {
     var config = getConfig();
     clearScene();
-    postionCamera(config.layers * 2.5)
+    BGL.camera.position(config.layers * 2.5);
     requestCells(config);
     start.textContent = 'Stop';
     start.classList.remove('green');
@@ -154,15 +192,15 @@ function toggleLife() {
 function togglePause() {
   var pause = document.getElementById('pause');
   if (pause.active) { // .active is the previous value as button transition has not happend yet
-    push({action: 'tick'})
+    push({action: 'tick'});
   }
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-  scene = createScene();
-  scene.add(createCenterCell());
-  grid = createAGrid(gridOptions);
-  scene.add(grid);
+  scene = BGL.createScene();
+  scene.add(BGL.cells.createCenter());
+  BGL.grid = createAGrid(BGL.gridOptions);
+  scene.add(BGL.grid);
   connectToSocket(requestCells);
 
   document.getElementById('start').addEventListener('click', toggleLife);
